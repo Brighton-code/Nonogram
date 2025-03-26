de8cf44b476edbc3b4a068e5eebfdae5ce96469b
Brighton <s1211945@student.windesheim.nl>
2025-03-26 10:54:11 +0100

    GameControl: Init

---
+++ Nonogram/Views/GameControl.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Nonogram.Views
+{
+    partial class GameControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // GameControl
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Margin = new Padding(0);
+            Name = "GameControl";
+            Size = new Size(500, 500);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
