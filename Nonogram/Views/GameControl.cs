04d7126bf7b91e3d2135cb8e63acf32248d66d3b
Brighton <s1211945@student.windesheim.nl>
2025-03-26 14:34:56 +0100

    made drawing correct with sizing

--- Nonogram/Views/GameControl.cs
+++ Nonogram/Views/GameControl.cs
@@ -32,16 +32,20 @@ namespace Nonogram.Views
 
         private void PnlGame_MouseClick(object? sender, MouseEventArgs e)
         {
-            if (e.X < 0 || e.X > _game.CellSize * _game.GridSize)
+            if (e.X < _game.GridStart.X || e.X - _game.GridStart.X > _game.GridStart.X + (_game.CellSize * _game.GridSize))
                 return;
-            if (e.Y < 0 || e.Y > _game.CellSize * _game.GridSize)
+            if (e.Y < _game.GridStart.Y || e.Y - _game.GridStart.Y > _game.GridStart.Y + (_game.CellSize * _game.GridSize))
                 return;
 
-            int col = (int)Math.Floor((float)e.X / _game.CellSize);
-            int row = (int)Math.Floor((float)e.Y / _game.CellSize);
+            int col = (int)Math.Floor(((float)e.X - _game.GridStart.X) / _game.CellSize);
+            int row = (int)Math.Floor(((float)e.Y - _game.GridStart.Y) / _game.CellSize);
+
+            MessageBox.Show($"{col}, {row}");
+
 
             if (_game.Marked[row, col] == 0) _game.Marked[row, col] = 1;
             else _game.Marked[row, col] = 0;
+
 
             pnlGame.Invalidate();
         }
@@ -54,15 +58,18 @@ namespace Nonogram.Views
 
             Graphics g = e.Graphics;
 
-            _game.CellSize = Math.Min(pnlGame.ClientSize.Width, pnlGame.ClientSize.Height) / _game.GridSize;
-            Rectangle area = new Rectangle(0, 0, _game.CellSize * _game.GridSize, _game.CellSize * _game.GridSize);
+            _game.CellSize = Math.Min(pnlGame.ClientSize.Width, pnlGame.ClientSize.Height) / (_game.GridSize + Math.Max(_game.RowHintMax, _game.ColHintMax));
+            _game.GridStart = new Point(_game.CellSize * _game.RowHintMax, _game.CellSize * _game.ColHintMax);
+            _game.GridArea = _game.CellSize * _game.GridSize;
+
+            Rectangle area = new Rectangle(_game.GridStart.X, _game.GridStart.Y, _game.GridArea, _game.GridArea);
 
             g.FillRectangle(Brushes.White, area);
-
+            
             for (int i = 0; i < _game.GridSize; i++)
             {
-                g.DrawLine(Pens.Black, 0, i * _game.CellSize, _game.CellSize * _game.GridSize, i * _game.CellSize);
-                g.DrawLine(Pens.Black, i * _game.CellSize, 0, i * _game.CellSize, _game.CellSize * _game.GridSize);
+                g.DrawLine(Pens.Black, _game.GridStart.X, _game.GridStart.Y + i * _game.CellSize, _game.GridStart.X + _game.GridArea, _game.GridStart.Y + i * _game.CellSize);
+                g.DrawLine(Pens.Black, _game.GridStart.X + i * _game.CellSize, _game.GridStart.Y, _game.GridStart.X + i * _game.CellSize, _game.GridStart.Y + _game.GridArea);
             }
             g.DrawRectangle(Pens.Black, area);
 
@@ -71,7 +78,7 @@ namespace Nonogram.Views
             for (int row = 0; row < _game.Solution.GetLength(0); row++)
                 for (int col = 0; col < _game.Solution.GetLength(1); col++)
                     if (_game.Solution[row, col] == 1)
-                        g.FillRectangle(Brushes.Red, col * _game.CellSize + _game.CellPadding.Left, row * _game.CellSize + _game.CellPadding.Top, _game.CellSize - _game.CellPadding.Left - _game.CellPadding.Right, _game.CellSize - _game.CellPadding.Bottom - _game.CellPadding.Top);
+                        g.FillRectangle(Brushes.Red, _game.GridStart.X + (col * _game.CellSize + _game.CellPadding.Left), _game.GridStart.Y + (row * _game.CellSize + _game.CellPadding.Top), _game.CellSize - _game.CellPadding.Left - _game.CellPadding.Right, _game.CellSize - _game.CellPadding.Bottom - _game.CellPadding.Top);
             // End Debug
 #endif
 
@@ -80,7 +87,7 @@ namespace Nonogram.Views
                 for (int col = 0; col < _game.Marked.GetLength(1); col++)
                 {
                     if (_game.Marked[row, col] == 1)
-                        g.FillRectangle(Brushes.Black, col * _game.CellSize + _game.CellPadding.Left, row * _game.CellSize + _game.CellPadding.Top, _game.CellSize - _game.CellPadding.Left - _game.CellPadding.Right, _game.CellSize - _game.CellPadding.Bottom - _game.CellPadding.Top);
+                        g.FillRectangle(Brushes.Black, _game.GridStart.X + (col * _game.CellSize + _game.CellPadding.Left), _game.GridStart.Y + (row * _game.CellSize + _game.CellPadding.Top), _game.CellSize - _game.CellPadding.Left - _game.CellPadding.Right, _game.CellSize - _game.CellPadding.Bottom - _game.CellPadding.Top);
                 }
             }
 
