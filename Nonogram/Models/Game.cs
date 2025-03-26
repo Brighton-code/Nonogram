5d48a76a54b7588e19b67be0fa6f85fa4908107f
Brighton <s1211945@student.windesheim.nl>
2025-03-26 12:06:13 +0100

    Game: Wrapper for game

---
+++ Nonogram/Models/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nonogram.Models
+{
+    public class Game
+    {
+        private int _gridSize;
+        public int GridSize 
+        {
+            get => _gridSize;
+            set
+            {
+                (Solution, Seed) = Grid.GenerateGrid(value);
+                (RowHints, ColHints) = Grid.CountSumsHorizontal(Solution);
+                _gridSize = value;
+            }
+        }
+
+        public int[,] Solution { get; private set; }
+        public int Seed { get; private set; }
+
+        public int[][] RowHints { get; private set; }
+        public int[][] ColHints { get; private set; }
+
+        public int _cellSize;
+
+        public Game(int size)
+        {
+            GridSize = size;
+        }
+    }
+}
