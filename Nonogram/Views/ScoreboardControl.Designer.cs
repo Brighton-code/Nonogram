8c0e6bdb405ee63074d038671eb8f1b74d719eea
bright <Brighton@vanrouendal.nl>
2025-04-03 13:51:20 +0200

    Scoreboard control start

---
+++ Nonogram/Views/ScoreboardControl.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Nonogram.Views
+{
+    partial class ScoreboardControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlLayout = new Panel();
+            SuspendLayout();
+            // 
+            // pnlLayout
+            // 
+            pnlLayout.Anchor = AnchorStyles.None;
+            pnlLayout.Location = new Point(0, 0);
+            pnlLayout.Margin = new Padding(0);
+            pnlLayout.Name = "pnlLayout";
+            pnlLayout.Size = new Size(500, 500);
+            pnlLayout.TabIndex = 0;
+            // 
+            // ScoreboardControl
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            Controls.Add(pnlLayout);
+            Margin = new Padding(0);
+            Name = "ScoreboardControl";
+            Size = new Size(500, 500);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlLayout;
+    }
+}
