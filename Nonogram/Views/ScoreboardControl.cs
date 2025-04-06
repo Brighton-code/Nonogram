917988447d38e1a6ae9269218dcc7fdaeac202a3
Ismael Winterman <noahh.lovell@gmail.com>
2025-04-06 12:01:12 +0200

    Get all game histories of the select size

--- Nonogram/Views/ScoreboardControl.cs
+++ Nonogram/Views/ScoreboardControl.cs
@@ -14,10 +14,17 @@ namespace Nonogram.Views
 {
     public partial class ScoreboardControl : UserControl, ILoadType
     {
+        List<int> allowedGridSizes = new List<int>()
+        {
+            5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+        };
+
         public ScoreboardControl()
         {
             InitializeComponent();
-            LoadType();
+            FillScoreboardDropdown();
+            cbSelectScoreboard.SelectedIndexChanged += GetGameHistories;
+            //LoadType();
         }
 
         public void LoadType()
@@ -34,5 +41,36 @@ namespace Nonogram.Views
                 }
             }
         }
+
+        private void FillScoreboardDropdown()
+        {
+            foreach(int size in allowedGridSizes)
+            {
+                cbSelectScoreboard.Items.Add($"{size}x{size} difficulty");
+            }
+        }
+
+        private List<GameHistory> GetGameHistories(object? sender, EventArgs e)
+        {
+            //int a = cbSelectScoreboard.SelectedIndex; 0 on 5x5
+            JsonUserDatabase db = new JsonUserDatabase();
+            List<User> users = db.GetUsers("../../../Database/Users.json");
+
+            List<GameHistory> allGames = new List<GameHistory>();
+
+            //Get all games
+            foreach (User user in users)
+            {
+                foreach (GameHistory history in user.History.Where(h => h.GridSize == allowedGridSizes.ElementAt(cbSelectScoreboard.SelectedIndex)))
+                {
+                    allGames.Add(history);
+                }
+            }
+
+            int a = 1;
+            return allGames;
+        }
+
+
     }
 }
